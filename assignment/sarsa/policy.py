992200ae7b1f61631566042262e1d70b83083f25
Joe MacMahon <joe@macmahon.eu>
2017-05-17 01:28:26 +0100

    Added SoftMax policy with decaying tau

--- assignment/sarsa/policy.py
+++ assignment/sarsa/policy.py
@@ -17,11 +17,23 @@ class EpsilonGreedy(Policy):
         super().__init__(num_actions)
         self._epsilon = epsilon
 
-    def choose_action(self, qs, t):
-        if np.random.rand() < self._epsilon:
+    def choose_action(self, qs, _):
+        if np.random.rand() < self._epsilon or np.allclose(qs[0], qs):
             action = np.random.randint(self._num_actions)
             self._logger.debug('Choosing randomly: {}'.format(action))
         else:
             action = np.argmax(qs)
             self._logger.debug('Choosing best: {}'.format(action))
         return action
+
+
+class SoftMaxDecay(Policy):
+    def __init__(self, num_actions, tau):
+        super().__init__(num_actions)
+        self._tau = tau
+
+    def choose_action(self, qs, t):
+        boltzmann_numerator = np.exp((t * qs) / self._tau)
+        probabilities = boltzmann_numerator / np.sum(boltzmann_numerator)
+
+        return np.random.choice(self._num_actions, p=probabilities)
